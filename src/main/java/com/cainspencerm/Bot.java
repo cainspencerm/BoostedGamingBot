7065a5a3cbcf041ea4075075427fe56173136ea1
Spencer <smc16s@my.fsu.edu>
2020-04-06 10:09:35 -0400

    Added jar file that works locally.

--- src/main/java/com/cainspencerm/Bot.java
+++ src/main/java/com/cainspencerm/Bot.java
@@ -7,25 +7,34 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
+import javax.security.auth.login.LoginException;
+
 public class Bot {
     static Role currentlyStreaming;
     static Guild boostedServer;
 
-    public static void main(String[] args) throws Exception {
-        JDA jda = JDABuilder
-                .create(
-                        GatewayIntent.GUILD_MEMBERS,
-                        GatewayIntent.GUILD_VOICE_STATES,
-                        GatewayIntent.GUILD_PRESENCES,
-                        GatewayIntent.GUILD_MESSAGES,
-                        GatewayIntent.GUILD_MESSAGE_TYPING,
-                        GatewayIntent.GUILD_MESSAGE_REACTIONS,
-                        GatewayIntent.GUILD_EMOJIS,
-                        GatewayIntent.DIRECT_MESSAGES,
-                        GatewayIntent.DIRECT_MESSAGE_REACTIONS,
-                        GatewayIntent.DIRECT_MESSAGE_TYPING
-                ).setToken("Njk2MTM1MjE1OTkwNTcxMDY4.XokUhg.lFEXy-A8SohgzlSWlTL2O8m3fCc").setActivity(Activity.of(Activity.ActivityType.DEFAULT, "Are you streaming for Boosted?")).build();
-        jda.addEventListener(new StreamListener());
+    public static void main(String[] args) {
+        recursiveJDA(null);
+    }
 
+    private static void recursiveJDA(JDA jda) {
+        try {
+            jda = JDABuilder
+                    .create(
+                            GatewayIntent.GUILD_MEMBERS,
+                            GatewayIntent.GUILD_VOICE_STATES,
+                            GatewayIntent.GUILD_PRESENCES,
+                            GatewayIntent.GUILD_MESSAGES,
+                            GatewayIntent.GUILD_MESSAGE_TYPING,
+                            GatewayIntent.GUILD_MESSAGE_REACTIONS,
+                            GatewayIntent.GUILD_EMOJIS,
+                            GatewayIntent.DIRECT_MESSAGES,
+                            GatewayIntent.DIRECT_MESSAGE_REACTIONS,
+                            GatewayIntent.DIRECT_MESSAGE_TYPING
+                    ).setToken("Njk2MTM1MjE1OTkwNTcxMDY4.XokUhg.lFEXy-A8SohgzlSWlTL2O8m3fCc").setActivity(Activity.of(Activity.ActivityType.DEFAULT, "Are you streaming for Boosted?")).build();
+            jda.addEventListener(new StreamListener());
+        } catch (LoginException e) {
+            recursiveJDA(jda);
+        }
     }
 }
