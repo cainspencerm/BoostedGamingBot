e5cbda122a10fb37b3babb48df7978a6352a80aa
Spencer <smc16s@my.fsu.edu>
2020-04-08 18:42:26 -0400

    Added rocket league role setter.

--- src/main/java/com/cainspencerm/RoleSetter.java
+++ src/main/java/com/cainspencerm/RoleSetter.java
@@ -12,42 +12,5 @@ import static com.cainspencerm.Bot.boostedServer;
 import static com.cainspencerm.Bot.delimiter;
 
 public class RoleSetter extends ListenerAdapter {
-    @Override
-    public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent event) {
-        if (Bot.boostedServer == null || Bot.currentlyStreaming == null) {
-            try {
-                Bot.boostedServer = event.getGuild();
-                Bot.currentlyStreaming = event.getGuild().getRolesByName("Currently Streaming", false).get(0);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
 
-        if (event.getAuthor().isBot()) {
-            return;
-        }
-
-        if (event.getMessage().getContentRaw().contains(delimiter + "add")) {
-            if (event.getMessage().getContentRaw().contains("rl")) {
-                try {
-                    boostedServer.addRoleToMember(Objects.requireNonNull(event.getMember()), boostedServer.getRolesByName("Boosted Rocket League", false).get(0)).queue();
-                    event.getChannel().sendMessage("Added Boosted Rocket League role to <@" + event.getAuthor().getId() + ">!").queue();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-        }
-
-        if (event.getMessage().getContentRaw().contains(delimiter + "remove")) {
-            if (event.getMessage().getContentRaw().contains("rl")) {
-                try {
-                    boostedServer.removeRoleFromMember(Objects.requireNonNull(event.getMember()), boostedServer.getRolesByName("Boosted Rocket League", false).get(0)).queue();
-                    event.getChannel().sendMessage("Removed Boosted Rocket League from to <@" + event.getAuthor().getId() + ">!").queue();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
 }
