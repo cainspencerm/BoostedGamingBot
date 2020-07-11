901fb7cdc16dad98b68c5ff9cb62a294b0e9878c
Spencer <smc16s@my.fsu.edu>
2020-07-11 14:26:53 -0400

    Added role setter reaction listener.

--- src/main/java/com/cainspencerm/Bot.java
+++ src/main/java/com/cainspencerm/Bot.java
@@ -12,23 +12,21 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Properties;
 
 public class Bot {
+    static JDA jda;
     static Role currentlyStreaming;
-    static Guild boostedServer;
-    static String gameReactionMessageId;
+    static Guild boostedServer /* = jda.getGuildById("679359965642883072") */ ;
+    static String gameReactionMessageId /* = "731198388346814476" */;
     static Properties prop;
     static String delimiter = "?";
 
-    public static void main(String[] args) {
-        recursiveJDA(null);
-    }
-
-    private static void recursiveJDA(JDA jda) {
+    public static void main(String[] args) throws LoginException {
         // Open guild file.
         prop = new Properties();
-        String fileName = "Guild.conf";
+        String fileName = "src/main/java/com/cainspencerm/Guild.conf";
         InputStream is = null;
 
         try {
@@ -38,36 +36,40 @@ public class Bot {
         }
 
         try {
+            assert is != null;
             prop.load(is);
         } catch (IOException e) {
             System.err.println("Could not load properties.");
         }
-        
-        try {
-            jda = JDABuilder
-                    .create(
-                            GatewayIntent.GUILD_MEMBERS,
-                            GatewayIntent.GUILD_VOICE_STATES,
-                            GatewayIntent.GUILD_PRESENCES,
-                            GatewayIntent.GUILD_MESSAGES,
-                            GatewayIntent.GUILD_MESSAGE_TYPING,
-                            GatewayIntent.GUILD_MESSAGE_REACTIONS,
-                            GatewayIntent.GUILD_EMOJIS,
-                            GatewayIntent.DIRECT_MESSAGES,
-                            GatewayIntent.DIRECT_MESSAGE_REACTIONS,
-                            GatewayIntent.DIRECT_MESSAGE_TYPING
-                    ).setToken("Njk2MTM1MjE1OTkwNTcxMDY4.XokUhg.lFEXy-A8SohgzlSWlTL2O8m3fCc").setActivity(Activity.of(Activity.ActivityType.DEFAULT, "Are you streaming for Boosted?")).build();
-            jda.addEventListener(new StreamListener());
-            jda.addEventListener(new RoleSetter());
-        } catch (LoginException e) {
-            recursiveJDA(jda);
-        }
-        
+
+
+        jda = JDABuilder
+                .create(
+                        GatewayIntent.GUILD_MEMBERS,
+                        GatewayIntent.GUILD_VOICE_STATES,
+                        GatewayIntent.GUILD_PRESENCES,
+                        GatewayIntent.GUILD_MESSAGES,
+                        GatewayIntent.GUILD_MESSAGE_TYPING,
+                        GatewayIntent.GUILD_MESSAGE_REACTIONS,
+                        GatewayIntent.GUILD_EMOJIS,
+                        GatewayIntent.DIRECT_MESSAGES,
+                        GatewayIntent.DIRECT_MESSAGE_REACTIONS,
+                        GatewayIntent.DIRECT_MESSAGE_TYPING
+                ).setToken("Njk2MTM1MjE1OTkwNTcxMDY4.XokUhg.lFEXy-A8SohgzlSWlTL2O8m3fCc").setActivity(Activity.of(Activity.ActivityType.DEFAULT, "Are you streaming for Boosted?")).build();
+        jda.addEventListener(new StreamListener());
+        jda.addEventListener(new RoleSetter());
+
         boostedServer = jda.getGuildById(prop.getProperty("boostedGuildId"));
-
-        assert boostedServer != null;
-        currentlyStreaming = boostedServer.getRolesByName("Currently Streaming", false).get(0);
-
         gameReactionMessageId = prop.getProperty("gameReactionMessageId");
+
+        List<Role> roleList = null;
+        while (roleList == null || roleList.isEmpty()) {
+            try {
+                roleList = boostedServer.getRolesByName("Currently Streaming", true);
+            } catch (Exception ignore) {
+                roleList = null;
+            }
+        }
+        currentlyStreaming = roleList.get(0);
     }
 }
--- src/main/java/com/cainspencerm/RoleSetter.java
+++ src/main/java/com/cainspencerm/RoleSetter.java
@@ -1,5 +1,7 @@
 package com.cainspencerm;
 
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
 import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionRemoveEvent;
@@ -8,7 +10,10 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import javax.annotation.Nonnull;
 
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 import static com.cainspencerm.Bot.*;
 
@@ -17,41 +22,101 @@ public class RoleSetter extends ListenerAdapter {
             "Sea of Thieves", "Borderlands", "Scribbl.io", "GTFO", "Animal Crossing",
             "Destiny", "RPGs", "Rust", "Minecraft", "Rocket League", "Modern Warfare"};
 
-    String[] games = {"csgo", "valorant", "pubg", "gtav", "overwatch", "sims", "seaofthieves", "borderlands", "scribblio", "gtfo", "animalcrossing", "destiny", "rpgs", "rust", "minecraft", "rl", "modernwarfare"};
+    String[] games = {"csgo", "valorant", "pubg", "gtav", "overwatch", "sims",
+            "seaofthieves", "borderlands", "scribblio", "gtfo", "animalcrossing",
+            "destiny", "rpgs", "rust", "minecraft", "rl", "modernwarfare"};
 
     @Override
     public void onGuildMessageReactionAdd(@Nonnull GuildMessageReactionAddEvent event) {
-        if (Bot.gameReactionMessageId.isEmpty() && event.getMember().getUser().getAsTag().equalsIgnoreCase("BG Mint#8834")) {
-            gameReactionMessageId = event.getMessageId();
-            Bot.prop.replace("gameReactionMessageId", event.getMessageId());
-        }
-
         if (event.getUser().isBot()) return;
 
         for (int i = 0; i < games.length; i++) {
-            if (event.getReactionEmote().getAsCodepoints().equalsIgnoreCase(":" + games[i] + ":")) {
+            if (event.getReactionEmote().getName().equalsIgnoreCase(games[i])) {
                 // Add role to member.
-                boostedServer.addRoleToMember(event.getMember(), boostedServer.getRolesByName(roles[i], true).get(0));
-                break;
+                try {
+                    Member member = event.getMember();
+                    List<Role> roleList = null;
+                    while (roleList == null || roleList.isEmpty()) {
+                        try {
+                            System.out.println("Rolelist troubleshooting.");
+                            roleList = boostedServer.getRolesByName(roles[i], true);
+                        } catch (Exception ignore) {
+                            roleList = null;
+                        }
+                    }
+
+                    Role role = roleList.get(0);
+                    boostedServer.addRoleToMember(member, role).queue();
+                    event.getChannel().sendMessage("Added role to " + member.getAsMention()).queue();
+
+                    String messageId = event.getChannel().getLatestMessageId();
+
+                    boostedServer.getTextChannelsByName("game-setter", true)
+                            .get(0)
+                            .deleteMessageById(messageId)
+                            .queueAfter(1, TimeUnit.MINUTES);
+                    return;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
+
+        // Role not added to member.
+        event.getChannel().sendMessage("Could not add role to " + event.getUser().getAsMention() + ". Try again in a few seconds.").queue();
+
+        String messageId = event.getChannel().getLatestMessageId();
+
+        boostedServer.getTextChannelsByName("game-setter", true)
+                .get(0)
+                .deleteMessageById(messageId)
+                .queueAfter(1, TimeUnit.MINUTES);
     }
 
     @Override
     public void onGuildMessageReactionRemove(@Nonnull GuildMessageReactionRemoveEvent event) {
-        if (Bot.gameReactionMessageId.isEmpty() && event.getMember().getUser().getAsTag().equalsIgnoreCase("BG Mint#8834")) {
-            gameReactionMessageId = event.getMessageId();
-            Bot.prop.replace("gameReactionMessageId", event.getMessageId());
-        }
-
         if (event.getUser().isBot()) return;
 
         for (int i = 0; i < games.length; i++) {
-            if (event.getReactionEmote().getAsCodepoints().equalsIgnoreCase(":" + games[i] + ":")) {
-                // Add role to member.
-                boostedServer.removeRoleFromMember(event.getMember(), boostedServer.getRolesByName(roles[i], true).get(0));
-                break;
+            if (event.getReactionEmote().getName().equalsIgnoreCase(games[i])) {
+                // Remove role from member.
+                try {
+                    Member member = event.getMember();
+                    List<Role> roleList = null;
+                    while (roleList == null || roleList.isEmpty()) {
+                        try {
+                            System.out.println("Rolelist troubleshooting.");
+                            roleList = boostedServer.getRolesByName(roles[i], true);
+                        } catch (Exception ignore) {
+                            roleList = null;
+                        }
+                    }
+
+                    Role role = roleList.get(0);
+                    boostedServer.removeRoleFromMember(member, role).queue();
+                    event.getChannel().sendMessage("Removed role from " + event.getUser().getAsMention()).queue();
+
+                    String messageId = event.getChannel().getLatestMessageId();
+
+                    boostedServer.getTextChannelsByName("game-setter", true)
+                            .get(0)
+                            .deleteMessageById(messageId)
+                            .queueAfter(1, TimeUnit.MINUTES);
+                    return;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
+
+        // Role not removed from member.
+        event.getChannel().sendMessage("Could not remove role from " + event.getUser().getAsMention() + ". Try again in a few seconds.").queue();
+
+        String messageId = event.getChannel().getLatestMessageId();
+
+        boostedServer.getTextChannelsByName("game-setter", true)
+                .get(0)
+                .deleteMessageById(messageId)
+                .queueAfter(1, TimeUnit.MINUTES);
     }
 }
